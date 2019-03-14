b76bc6cac72b30560303d048ac1355bdd662c989
dolaku <dolaku@gmail.com>
2019-03-13 22:41:43 -0400

    Check wrong letter input - WIP

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,12 +1,16 @@
+// Word Bank
+var words = ['pancake', 'waffle', 'sausage', 'eggs', 'omelette', 'muffin', 'bagel', 'toast', 'bacon', 'yogurt', 'oatmeal', 'grits', 'cereal', 'biscuit', 'croissant', 'juice', 'strawberry', 'blueberry', 'blackberry', 'raspberry'];
 var warning = document.getElementById('warning');
 var hangman = document.getElementById('hangman');
 var guessWrong = document.getElementById('guessWrong');
 var win = document.getElementById('win');
 var lose = document.getElementById('lose');
 var guessRemaining = document.getElementById('guessRemaining');
+var winCount = 0;
+var loseCount = 0;
+var placeholderArray = [];
+var lettersArray = [];
 
-// Word Bank
-var words = ['pancake', 'waffle', 'sausage', 'eggs', 'omelette', 'muffin', 'bagel', 'toast', 'bacon', 'yogurt', 'oatmeal', 'grits', 'cereal', 'biscuit', 'croissant', 'juice', 'strawberry', 'blueberry', 'blackberry', 'raspberry'];
 // Transforms words to uppercase
 for (var u = 0; u < words.length; u++) {
     words[u] = words[u].toUpperCase();
@@ -15,15 +19,19 @@ for (var u = 0; u < words.length; u++) {
 // Choose word randomly based on items in food array
 var randomNum = Math.floor(Math.random() * words.length);
 var randomWord = words[randomNum];
+var randomWordSplit = randomWord.split('');
 
-// Underscores based on word length
-var placeholderArray = [];
+// Blank spaces based on word length
 for (var i = 0; i < randomWord.length; i++) {
     placeholderArray[i] = '<div class="hangman-letters"><span>' + randomWord[i] + '</span></div>';
     for (var j = 0; j < placeholderArray.length; j++) {
         var blockLetters = placeholderArray[j] + ' ';
     }
     hangman.innerHTML += blockLetters;
+
+    // create guesses remaining depending on length of word
+    guessRemCount = randomWordSplit.length + 3;
+    guessRemaining.innerHTML = guessRemCount;
 }
 
 // Initialize game
@@ -32,6 +40,7 @@ function game() {
     var inputLetter = guessLetter.toUpperCase();
     var remainingLetters = randomWord.length;
     var wrongArray = [];
+    var wrongPlaceholder = [];
 
     // prevents page from refreshing on submit
     event.preventDefault();
@@ -39,15 +48,31 @@ function game() {
     // check input is a letter - not a number or symbol
     if (!/^([A-Z])$/.test(inputLetter)) {
         warning.innerHTML = 'Please enter a letter.';
-        guessWrong.innerHTML = inputLetter; //for testing
     } else {
         warning.innerHTML = '';
 
-        // if wrong - Push incorrect letters to wrong-array
-        /* if () {
+        // loop through each letter
+        for (var x = 0; x < randomWordSplit.length; x++) {
+            if (inputLetter !== randomWordSplit[x]) {
+                //  - Push incorrect letters to wrong-array
+                wrongArray.push(inputLetter);
+                for (var y = 0; y < wrongArray.length; y++) {
+                    wrongPlaceholder[y] = '<div class="wrong-letters"><span>' + wrongArray[y] + '</span></div>';
+                    for (var z = 0; z < wrongPlaceholder.length; z++) {
+                        var wrongBlockLetters = wrongPlaceholder[z];
+                    }
+                    guessWrong.innerHTML = wrongBlockLetters;
 
-        } */
-        guessWrong.innerHTML = inputLetter; // for testing
+            } /* else if (inputLetter === randomWordSplit[x]) {
+                console.log(randomWordSplit[x]);
+                guessWrong.innerHTML = inputLetter + 'yyy'; // for testing */
+            }
+        }
+
+
+            //  - Check if letter was already guessed
+            //  - Decrease guessRemCount
+
 
 
         // if correct - Push correct letters to replace that underscore
@@ -63,4 +88,4 @@ function game() {
 
 
 console.log(randomWord); //for testing
-console.log(placeholderArray); //for testing
+console.log(randomWordSplit); //for testing
